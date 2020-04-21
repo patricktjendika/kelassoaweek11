62e423050816495305b06626f99e7e14e0140a69
patricktjendika <patricktjendika2521999@gmail.com>
2020-04-21 13:14:38 +0700

    Testing

--- app.js
+++ app.js
@@ -206,5 +206,5 @@ app.put('/updatePlaylist/:idplaylist', (req, res) => {
     res.send(obj);
 });
  
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port '+port+'...'));
